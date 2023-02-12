f3f3e2fa9aa59277e3232f717cb023ccbcc2ce84
Annika Schrenk <schrenkannika@gmail.com>
2023-02-12 15:48:17 +0000

    Slider/mousedrag works

--- script.js
+++ script.js
@@ -11,8 +11,29 @@ carousel.addEventListener("mousemove", dragging);
 
 let slider = tns({
     container : ".my-slider",
-    "slideBy" : "1",
+    "slideBy" : "page",
     "speed" : 400,
     "nav" : false,
-
+    "mouseDrag" : true,
+    "swipeAngle" : false,
+    autoplay : false,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        1600: {
+            items : 4,
+            gutter : 20
+        },
+        1024: {
+            items: 3,
+            gutter: 20
+        },
+        768: {
+            items: 2,
+            gutter : 20
+        },
+        480: {
+            items : 1
+        },
+    }
 })
